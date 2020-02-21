306f97f9317ce6ca289e10c9f3188db05102260b
FelipeEnne <felipeenne@gmail.com>
2020-02-21 16:54:34 -0300

    adding the playturn

--- js/index.js
+++ js/index.js
@@ -8,9 +8,10 @@ const gameBoard = (space, value) => {
 
 const player = (name, symbol) => {
   const victor = () => console.log(name + " won!!!");
-
   return{ name, symbol, victor };
 }
+
+let playTurn = 1;
 
 function init() {
   const board = gameBoard();
@@ -28,15 +29,17 @@ function updateSpace(board, space, value){
 }
 
 
-
 //updateSpace(board, 5, 1);
 //updateSpace(board, 0, 2);
 //console.log(player1);
 //console.log(player2);
 
 function space(value){
-  console.log(value);
-  return 0;//change
+  updateSpace(board, value, playTurn);
+  c = checkBoard(playTurn);
+  playTurn == 1 ? playTurn = 2 : playTurn = 1;
+  console.log(board)
+  return playTurn;
 }
 
 function checkBoard(play){
@@ -57,34 +60,38 @@ function checkBoard(play){
   return false;
 }
 
+
 function playGame(){
   board = init();
 
-  let play = 0;
 
   player1 = initPlayers('Raphael', 'X');
   player2 = initPlayers('Felipe', 'O');
 
-  let c = checkBoard(play);
+}
 
-  while(c == false){
-    if(play === 0){
-      s = space();
-      updateSpace(board, s, 1);
-      c = checkBoard(play);
-      play = 1;
-    }
-    else{
-      s = space();
-      updateSpace(board, s, 2);
-      c = checkBoard(play);
-      play = 0;
-    }
-  }
-
+function render() {
+  let tableRows = "";
+  board.forEach(i => {
+    tableRows += `<tr class="table-rows">
+                    <th class="board-border text-center" onclick="space(0)"></th>
+                    <th class="board-border text-center background-X" onclick="space(1)"></th>
+                    <th class="board-border text-center background-O"></th>
+                  </tr>
+                  <tr class="table-rows">
+                    <th class="board-border text-center">3</th>
+                    <th class="board-border text-center">4</th>
+                    <th class="board-border text-center">5</th>
+                  </tr>
+                  <tr class="table-rows">
+                    <th class="board-border text-center">6</th>
+                    <th class="board-border text-center">7</th>
+                    <th class="board-border text-center">8</th>
+                  </tr>`;
+  });
+  document.getElementById("table-rows").innerHTML = tableRows;
 }
 
 
 playGame()
-
 console.log(board);
