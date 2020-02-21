575492b07c7847501dd443ebc97b9b514f575c0a
FelipeEnne <felipeenne@gmail.com>
2020-02-21 14:36:09 -0300

    creating the game logic

--- js/index.js
+++ js/index.js
@@ -27,13 +27,63 @@ function updateSpace(board, space, value){
   board.changeSpace(space, value);
 }
 
-board = init();
-player1 = initPlayers('Raphael', 'X');
-player2 = initPlayers('Felipe', 'O');
-updateSpace(board, 5, 1);
-updateSpace(board, 0, 2);
-console.log(player1);
-console.log(player2);
-// console.log(player[1].name);
-// player[0].victor();
+
+
+//updateSpace(board, 5, 1);
+//updateSpace(board, 0, 2);
+//console.log(player1);
+//console.log(player2);
+
+function space(){
+  return 0;//change
+}
+
+function checkBoard(play){
+
+  for(let i = 0; i <= 2 ; i++){
+    if(board[i*3] == board[i*3+1] && board[i*3+1] == board[i*3+2] || 
+      board[i] == board[i+3] && board[i+6] == board[i+3]){
+         return true
+      }
+  }
+
+  if(board[0] == board[4] && board[8] == board[4] || 
+    board[2] == board[4] && board[6] == board[4]){
+       return true
+  }
+  
+
+  return false;
+}
+
+function playGame(){
+  board = init();
+
+  let play = 0;
+
+  player1 = initPlayers('Raphael', 'X');
+  player2 = initPlayers('Felipe', 'O');
+
+  let c = checkBoard(play);
+
+  while(c == false){
+    if(play === 0){
+      s = space();
+      updateSpace(board, s, 1);
+      c = checkBoard(play);
+      play = 1;
+    }
+    else{
+      s = space();
+      updateSpace(board, s, 2);
+      c = checkBoard(play);
+      play = 0;
+    }
+  }
+
+}
+
+
+playGame()
+
 console.log(board);
